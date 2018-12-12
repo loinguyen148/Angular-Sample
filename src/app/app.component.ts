89cb5125fe810814324abc6d53e7200aca623f49
Son Nguyen <max.nguyen@terralogic.com>
2018-12-12 15:34:35 +0700

    Update CRUD hero functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
-export interface Hero { name: string; }
+export interface Hero { id: string, name: string; }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
   heroes: Observable<Hero[]>;
   selectedHero: Hero;
   private heroesCollection: AngularFirestoreCollection<Hero>;
   
-  constructor(db: AngularFirestore) {
-    this.heroesCollection = db.collection<Hero>('heroes');
-    this.heroes = db.collection<Hero>('heroes').valueChanges();
+  constructor(private db: AngularFirestore) {
+    
+  }
+  
+  ngOnInit(): void {
+    this.getHeroes();
+    this.heroes.subscribe(hero => {
+      console.warn(hero);
+    });
+  }
+  
+  getHeroes(): void {
+    this.heroesCollection = this.db.collection<Hero>('heroes');
+    this.heroes = this.db.collection<Hero>('heroes').valueChanges();
   }
   
   selectHero(hero: Hero) {
@@ -26,21 +37,40 @@ export class AppComponent {
   
   addHero(heroName: string) {
     if (heroName && heroName.trim().length) {
-      const newHero: Hero = {
+      const id = this.db.createId();
+      const hero: Hero = {
+        id,
         name: heroName
       };
       
-      this.heroesCollection.add(newHero);
+      this.heroesCollection.doc(id).set(hero).then(() => {  
+        console.log("Document successfully added!");
+      }).catch((error) => {
+        console.error("Error adding document: ", error);
+      });
+      // this.heroesCollection.doc(id).set(hero); // Another way to add new item
     }
   }
   
   updateHero(hero: Hero) {
-    console.warn(this.selectedHero);
-    // this.heroesCollection.doc(this.selectedHero.name).update({ name: 'updated name' });
-    this.heroesCollection.doc(this.selectedHero.name).update({ name: 'updated name' });
+    if (hero) {
+      hero.name = 'Updated ' + hero.name;
+      this.heroesCollection.doc(hero.id).update(hero);
+      const currentHero: any = this.heroesCollection.doc(hero.id).ref.get();
+      
+      console.warn(currentHero.__zone_symbol__value.id);
+    }
   }
   
   deleteHero(hero: Hero) {
-    this.heroesCollection.doc(hero.name).delete();
+    if (hero) {
+      this.heroesCollection.doc(hero.id).delete().then(() => {
+        console.log("Document successfully deleted!");
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+    
+    this.selectedHero = {} as Hero;
+    }
   }
 }
